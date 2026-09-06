d4cf73e940c9bf2856b770954001a225ae18e79f
BABISANKARI <104904061+BABISANKARI@users.noreply.github.com>
2026-09-06 12:06:48 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -41,10 +41,11 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
+   /* {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
+    */
 
     {
       name: 'webkit',
